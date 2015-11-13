6ea579fe8ac79eade903adfbd552a2cbb2457bd4
Lujun Jian <ljian@smith.edu>
2015-11-12 20:23:37 -0500

    Version one. Draw background with off-screen buffer.

---
+++ custom_game_loop.js
@@ -0,0 +1,71 @@
+// <editor-fold desc="CustomGameLoop">
+function CustomGameLoop() {
+    this.elementManager = new ElementManager();
+}
+
+CustomGameLoop.prototype = new GameLoop();
+
+CustomGameLoop.prototype.generateFakePointData = function(f, n, color) {
+    var returnData = new PointData();
+    returnData.setColor(color);
+    var step = 1000 / n;
+    var randomSpread = 200;
+    for (var i = 0; i < n; i++) {
+        var x = i * step;
+        var point = 
+                new Point(
+                    x + (Math.random() * randomSpread - randomSpread / 2), 
+                    f(x) + (Math.random() * randomSpread - randomSpread / 2));
+        returnData.addPoint(point);
+    }
+    return returnData;
+}
+
+CustomGameLoop.prototype.initialize = function(canvas) {
+    GameLoop.prototype.initialize.call(this, canvas);
+    // add initialization here
+}
+
+CustomGameLoop.prototype.draw = function(g) {
+    g.fillStyle = "black";
+    g.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.elementManager.draw(g);
+//    var backgroundImage = new Image();
+//    backgroundImage.src = "voyager.png";
+//    g.drawImage(backgroundImage, 300, 300);
+    // add drawing here
+}
+CustomGameLoop.prototype.addElement = function(e){
+    this.elementManager.addElement(e);
+}
+// </editor-fold>
+
+function initialize() {
+    var customGameLoop = new CustomGameLoop();
+    customGameLoop.initialize(document.getElementById("canvas"));
+    
+    var starField = new StarField();   
+    starField.setDistance(1, 1000, 1);
+    //starField.createStars();
+    customGameLoop.addElement(starField); 
+    
+    var starField2 = new StarField();   
+    starField2.setDistance(2, 500, 1.5);
+    //starField2.createStars();
+    customGameLoop.addElement(starField2); 
+    
+    var voyager = new Spaceship();
+    voyager.setURL("voyager.png");
+    
+    customGameLoop.addElement(voyager);
+    
+    var starField3 = new StarField();   
+    starField3.setDistance(4, 300, 2.5);
+    //starField3.createStars();
+    customGameLoop.addElement(starField3); 
+    
+}
+
+window.onload = initialize;
+
+
---
+++ elements.js
@@ -0,0 +1,192 @@
+function Visual(){
+    this.position;
+};
+Visual.prototype.setPosition = function(p){
+    this.position = p;
+};
+
+Visual.prototype.getPosition = function(){
+    return this.position;
+};
+
+Visual.prototype.draw = function(g){
+    
+};
+
+function Star(){
+    this.size; 
+    this.color;
+}
+
+Star.prototype = new Visual();
+
+Star.prototype.setSize = function(s){
+    this.size = s;
+};
+
+Star.prototype.getSize = function(){
+    return this.size;
+};
+
+Star.prototype.setColor = function(c){
+    this.color = c;
+}
+
+Star.prototype.draw = function(g){
+    g.beginPath();
+    g.arc(this.position.getX(), this.position.getY(), this.size, 0, 2*Math.PI);
+    g.fillStyle = this.color;
+    g.fill();
+    g.strokeStyle = this.color;
+    g.stroke();
+};
+
+function StarField(){
+    this.speed;
+    this.density;
+    this.starSize;
+    this.buffer;
+    this.bufferG;
+    this.stars = [];
+    this.drawOnBuffer = true;
+    this.startX = 0;
+}
+
+StarField.prototype = new Visual();
+
+
+StarField.prototype.setSpeed = function(s){
+    this.speed = s;
+}
+
+StarField.prototype.setDensity = function(d){
+    this.density = d;
+}
+
+StarField.prototype.setStarSize = function(s){
+    this.starSize = s;
+}
+
+StarField.prototype.setDistance = function(s, d, size){
+    this.setSpeed(s);
+    this.setDensity(d);
+    this.setStarSize(size);
+}
+StarField.prototype.setStarWidth = function(s){
+    this.starWidth = s;
+}
+
+StarField.prototype.setStarHeight = function(s){
+    this.starHeight = s;
+}
+
+StarField.prototype.generateRandomPosition = function(){
+    var x = Math.round(Math.random()*(this.buffer.width/2));
+    var y = Math.round(Math.random()*(this.buffer.height));
+    return new Point(x, y);
+}
+
+StarField.prototype.createStars = function(){
+    for(var i = 0 ; i < this.density; i++){
+        var position = this.generateRandomPosition();
+        var color = this.generateColor();
+        var star = new Star();
+        star.setColor(color);
+        star.setPosition(position);
+        star.setSize(this.starSize);
+        this.stars.push(star);
+        
+        var star2 = new Star();
+        star2.setColor(color);
+        var x = position.getX() + this.buffer.width/2;
+        var newp = new Point(x, position.getY());
+        star2.setPosition(newp);
+        star2.setSize(this.starSize);
+        this.stars.push(star2);
+    }
+}
+
+StarField.prototype.generateColor = function(){
+    var colorArray = ["white", "#82CAFA", "#FFFFC2", "#9F000F"];
+    var color;
+    var num = Math.random()*10;
+    if(num <=7){
+       color = colorArray[0]; 
+    } else if (num < 8){
+       color = colorArray[1];        
+    } else if (num < 9){
+       color = colorArray[2];        
+    } else if (num < 10){
+       color = colorArray[3];        
+    }
+    return color;
+    
+    
+}
+
+StarField.prototype.createBuffer = function(){
+    this.buffer = document.createElement('canvas');
+    var canvas = document.getElementById('canvas');
+    
+    this.buffer.width = canvas.width*2;
+    this.buffer.height = canvas.height;
+    
+    this.bufferG = this.buffer.getContext('2d');
+    
+    this.createStars();
+    
+    for(var i=0 ; i < this.stars.length; i++){
+        this.stars[i].draw(this.bufferG);
+    }
+};
+
+StarField.prototype.draw = function(g){
+    if(this.drawOnBuffer === true){
+        this.createBuffer();   
+        this.drawOnBuffer = false;
+    };
+    
+    g.save();
+    g.translate(this.startX, 0);   
+    
+    
+    g.drawImage(this.buffer, 0, 0);    
+    g.restore();
+    
+    if( this.startX > (0 - this.buffer.width/2)){
+        this.startX -= this.speed;
+    } else {
+        this.startX = 0;
+    }
+    
+    
+
+}
+
+function Spaceship(){
+    this.url;
+    
+}
+Spaceship.prototype = new Visual();
+Spaceship.prototype.setURL = function(u){
+    this.url = u;
+}
+Spaceship.prototype.draw = function(g){
+    var backgroundImage = new Image();
+    backgroundImage.src = this.url;
+    g.drawImage(backgroundImage, 300, 300);
+}
+
+function ElementManager(){
+    this.elements = [];
+}
+
+ElementManager.prototype.addElement = function(e){
+    this.elements.push(e);
+}
+ElementManager.prototype.draw = function(g){
+    for(var i = 0; i < this.elements.length; i++){
+        this.elements[i].draw(g);
+    }
+}
+
